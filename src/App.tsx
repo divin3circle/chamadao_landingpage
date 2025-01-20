1b04aee16e9b80e1fa6c87ad6eae2108ca4808a3
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2025-01-20 05:11:41 +0300

    feat: implement no-scroll class for overlay management and update overlay height

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useOverlayStore } from "./hooks/overlayStore";
 import Contact from "./pages/Contact";
 import Error from "./pages/Error";
@@ -8,10 +9,19 @@ import { ToastContainer } from "react-toastify";
 
 export default function App() {
   const { isOverlayOpen } = useOverlayStore();
+
+  useEffect(() => {
+    if (isOverlayOpen) {
+      document.body.classList.add("no-scroll");
+    } else {
+      document.body.classList.remove("no-scroll");
+    }
+  }, [isOverlayOpen]);
+
   return (
     <BrowserRouter>
       {isOverlayOpen && (
-        <div className="w-full absolute inset-0 bottom-0 top-0 h-[100%] bg-black/70"></div>
+        <div className="w-full absolute inset-0 bottom-0 top-0 h-[100dvh] bg-black/70"></div>
       )}
       <Routes>
         <Route path="/" element={<Landing />} />
